2408443423c086c783c602a81aae4e2991070790
Eldorbek2233 <eldorbekxakimxujayev4@gmail.com>
2025-07-14 22:21:09 +0500

    Fix bot.py: correct handler references to existing functions in handlers.py

--- bot.py
+++ bot.py
@@ -49,61 +49,21 @@ def setup_handlers(application):
     """Setup all bot handlers"""
     # Set up handlers
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("users", users_count))
-    application.add_handler(CommandHandler("stat", bot_stat))
+    application.add_handler(CommandHandler("stat", stat))
     application.add_handler(CommandHandler("admin_menu", admin_menu))
     application.add_handler(CommandHandler("help", help_command))
+    # Reklama conversation handler
     application.add_handler(ConversationHandler(
-        entry_points=[
-            CommandHandler("reklama", send_advertisement), 
-            CallbackQueryHandler(reklama_inline_handler, pattern="^reklama_inline$")
-        ],
+        entry_points=[CommandHandler("reklama", send_advertisement)],
         states={
-            REKLAMA_WAIT: [MessageHandler(filters.ALL & ~filters.COMMAND, handle_ad_content)]
+            REKLAMA_WAIT: [MessageHandler(filters.TEXT | filters.PHOTO, handle_ad_content)]
         },
-        fallbacks=[CommandHandler("cancel", cancel_handler)],
+        fallbacks=[CommandHandler("cancel", cancel_command)],
         per_message=False
     ))
     
-    # Kino joylash uchun conversation handler
-    application.add_handler(ConversationHandler(
-        entry_points=[
-            CommandHandler("add_movie", add_movie_handler),
-            CallbackQueryHandler(add_movie_handler, pattern="^add_movie$"),
-            MessageHandler(filters.Regex(r"(?i)^🎬?\s*kino\s*joylash$"), add_movie_handler)
-        ],
-        states={
-            WAITING_FOR_CODE: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_movie_code)],
-            WAITING_FOR_VIDEO: [MessageHandler(filters.VIDEO | filters.ANIMATION, get_movie_video)]
-        },
-        fallbacks=[CommandHandler("cancel", cancel_handler)],
-        per_message=False
-    ))
-    
-    # Kanal boshqarish uchun conversation handler
-    application.add_handler(ConversationHandler(
-        entry_points=[
-            CommandHandler("manage_channels", manage_channels),
-            CallbackQueryHandler(manage_channels, pattern="^manage_channels$"),
-            MessageHandler(filters.Regex(r"(?i)^📢?\s*kanallar$"), manage_channels)
-        ],
-        states={
-            CHANNEL_MENU: [
-                CallbackQueryHandler(add_channel_command, pattern="^add_channel$"),
-                CallbackQueryHandler(remove_channel_command, pattern="^remove_channel$"),
-                CallbackQueryHandler(back_to_menu, pattern="^back_to_menu$"),
-                CallbackQueryHandler(delete_channel, pattern="^delete_channel_[0-9]+$")
-            ],
-            WAITING_FOR_CHANNEL: [MessageHandler(filters.TEXT & ~filters.COMMAND, process_new_channel)]
-        },
-        fallbacks=[CommandHandler("cancel", cancel_handler)],
-        per_message=False
-    ))
-    
-    application.add_handler(MessageHandler((filters.TEXT | filters.PHOTO) & ~filters.COMMAND, handle_code))
-    application.add_handler(MessageHandler(filters.ALL, handle_channel_video))
-    application.add_handler(CallbackQueryHandler(confirm_membership, pattern="^check_membership$"))
-    application.add_handler(CallbackQueryHandler(stat_button_handler, pattern="^show_stat$"))
+    # Matn xabarlar uchun handler
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     
     # Error handler qo'shish
     from handlers import error_handler
