82ba0da074c306d5aa219f9e9ac0f5fb710b9135
Eldorbek2233 <eldorbekxakimxujayev4@gmail.com>
2025-07-14 23:39:53 +0500

    Fix handle_message: use handle_code instead of non-existent handle_message

--- handlers.py
+++ handlers.py
@@ -800,7 +800,7 @@ def add_handlers(app):
     app.add_handler(reklama_conv_handler)
     
     # Matn xabarlar uchun handler
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_code))
     
     # Error handler
     app.add_error_handler(error_handler)
